48757bc09ecef70c29e12cccc9a6d19c7e5a4660
Anushka Varshney <anushkavarshney2708@gmail.com>
2026-01-08 21:42:03 +0530

    set response headers and status code

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const server = http.createServer(async (req, res) => {
   console.log(req.url);
   const destinationsData = await getDataFromDB();
   if (req.url === "/api" && req.method === "GET") {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 200;
     res.end(JSON.stringify(destinationsData));
   }
 });
