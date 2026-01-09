c7d424840549e058c79385edd933504700e8f79e
Anushka Varshney <anushkavarshney2708@gmail.com>
2026-01-09 21:07:13 +0530

    specified access control methods

--- utils/sendJSONResponse.js
+++ utils/sendJSONResponse.js
@@ -1,5 +1,7 @@
 export const sendJSONResponse = (res, status, payload) => {
   res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
   res.statusCode = status;
   res.end(JSON.stringify(payload));
 };
